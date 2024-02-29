2dbe544f127e8cc67df0d3dad910e3dab62a3b8f
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-02-29 12:12:53 +0100

    Test code for week 2 - will be removed after handing in

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,17 +1,84 @@
 #include "stdafx.h"
 #include "InputManager.h"
 
+#include "GameObject.h"
+#include "SceneManager.h"
+
+void dae::InputManager::Init()
+{
+}
+
 bool dae::InputManager::ProcessInput()
 {
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT) {
+		if (e.type == SDL_QUIT) 
+		{
 			return false;
 		}
-		if (e.type == SDL_KEYDOWN) {
-			
+		if (e.type == SDL_KEYDOWN) 
+		{
+			if (e.key.keysym.scancode == SDL_SCANCODE_1)
+			{
+				const auto pChild = SceneManager::GetInstance().GetActiveScene().GetGameObject(m_ChildId);
+				if (pChild == nullptr)
+				{
+					Logger::LogInfo({ "Child Object no longer exists" });
+					return true;
+				}
+					
+				// DETACH CHILD
+				pChild->SetParent(nullptr, true);
+				Logger::LogInfo({ "Detached child ( id: {} ) from parent ( id: {} )" }, m_ParentId, m_ChildId);
+			}
+			if (e.key.keysym.scancode == SDL_SCANCODE_2)
+			{
+				const auto pChild = SceneManager::GetInstance().GetActiveScene().GetGameObject(m_ChildId);
+				if (pChild == nullptr)
+				{
+					Logger::LogInfo({ "Child Object no longer exists" });
+					return true;
+				}
+				const auto pParent = SceneManager::GetInstance().GetActiveScene().GetGameObject(m_ParentId);
+				if (pParent == nullptr)
+				{
+					Logger::LogInfo({ "Parent Object no longer exists" });
+					return true;
+				}
+
+				// ATTACH CHILD
+				pChild->SetParent(pParent, true);
+				Logger::LogInfo({ "Attached child ( id: {} ) to parent ( id: {} )" }, m_ChildId, m_ParentId);
+			}
+			if (e.key.keysym.scancode == SDL_SCANCODE_3)
+			{
+				const auto pChild = SceneManager::GetInstance().GetActiveScene().GetGameObject(m_ChildId);
+				if (pChild == nullptr)
+				{
+					Logger::LogInfo({ "Child Object no longer exists" });
+					return true;
+				}
+
+				// DESTROY CHILD
+				pChild->Destroy();
+				Logger::LogInfo({ "Destroyed child ( id: {} )" }, pChild->GetId());
+			}
+			if (e.key.keysym.scancode == SDL_SCANCODE_4)
+			{
+				const auto pParent = SceneManager::GetInstance().GetActiveScene().GetGameObject(m_ParentId);
+				if (pParent == nullptr)
+				{
+					Logger::LogInfo({ "Parent Object no longer exists" });
+					return true;
+				}
+
+				// DESTROY PARENT
+				pParent->Destroy();
+				Logger::LogInfo({ "Destroyed parent ( id: {} )" }, pParent->GetId());
+			}
 		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
+		if (e.type == SDL_MOUSEBUTTONDOWN)
+		{
 			
 		}
 		// etc...
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -5,10 +5,16 @@
 
 namespace dae
 {
+	class GameObject;
+
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
+		void Init() override;
 		bool ProcessInput();
+
+		// TODO: Delete this after week 2, this is for testing only
+		UINT m_ParentId, m_ChildId;
 	};
 
 }
