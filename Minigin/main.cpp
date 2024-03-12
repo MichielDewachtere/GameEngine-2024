4223011b8e82d9a419f956efb7c089c21c0f57dd
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-12 11:59:57 +0100

    Finised week 04 assignment

--- Minigin/main.cpp
+++ Minigin/main.cpp
@@ -10,6 +10,7 @@
 #include "TrashTheCache.h"
 #include "Rotator.h"
 #include "FPSCounter.h"
+#include "MoveCommand.h"
 
 void load()
 {
@@ -19,7 +20,6 @@ void load()
 
 	auto backgroundTex = ResourceManager::GetInstance().LoadTexture("background.tga");
 	auto& backGround = scene.CreateGameObject({});
-	//backGround.AddComponent<TextureComponent>("background_debug.png");
 	backGround.AddComponent<TextureComponent>(std::move(backgroundTex));
 
 	auto logoTex = ResourceManager::GetInstance().LoadTexture("logo.tga");
@@ -36,7 +36,7 @@ void load()
 	auto fontSmall = ResourceManager::GetInstance().LoadFont("Lingua.otf", 24);
 	auto& fpsCounter = scene.CreateGameObject();
 	fpsCounter.AddComponent<FPSCounter>();
-	fpsCounter.AddComponent<TextComponent>("00", std::move(fontSmall), SDL_Color{ 255,255,0,255 });;
+	fpsCounter.AddComponent<TextComponent>("00", std::move(fontSmall), SDL_Color{ 255,255,0,255 });
 	fpsCounter.GetTransform()->SetLocalPosition(0, 0);
 
 	auto characterOne = ResourceManager::GetInstance().LoadTexture("1.png");
@@ -47,9 +47,6 @@ void load()
 	const auto textureComp = parent.AddComponent<TextureComponent>(std::move(characterOne));
 	textureComp->SetRenderOffset(8, 8);
 	parent.GetTransform()->SetLocalPosition(640 / 2.f + 20, 480 / 2.f + 20);
-	parent.AddComponent<Rotator>(glm::vec2{ 640 / 2.f, 480 / 2.f }, 2.f);
-
-	InputManager::GetInstance().m_ParentId = parent.GetId();
 
 	auto& child = parent.CreateGameObject();
 	const auto textureComp1 = child.AddComponent<TextureComponent>(std::move(characterTwo));
@@ -57,22 +54,29 @@ void load()
 	child.GetTransform()->SetLocalPosition(20, 20);
 	child.AddComponent<Rotator>(glm::vec2{ 0,0 }, 10.f);
 
-	InputManager::GetInstance().m_ChildId = child.GetId();
-
-	auto& secondChild = parent.CreateGameObject();
-	const auto textureComp2 = secondChild.AddComponent<TextureComponent>(std::move(characterThree));
+	auto& parent1 = scene.CreateGameObject();
+	const auto textureComp2 = parent1.AddComponent<TextureComponent>(std::move(characterThree));
 	textureComp2->SetRenderOffset(8, 8);
-	secondChild.GetTransform()->SetLocalPosition(30, 30);
-	secondChild.AddComponent<Rotator>(glm::vec2{ 0,0 }, 12.5f);
+	parent1.GetTransform()->SetLocalPosition(640 / 2.f + 50, 480 / 2.f + 20);
 
-	//auto& imGuiTest = scene.CreateGameObject();
-	//imGuiTest.AddComponent<TrashTheCache>();
+	auto& input = InputManager::GetInstance();
+	input.EnableGamePadInput(true);
+	const auto id = input.RegisterGamePad();
+	const auto map = input.AddInputMap("test");
+	input.SetInputMapActive("test");
+	map->AddKeyboardAction<MoveCommand>(0, KeyState::keyPressed, SDL_SCANCODE_W, &parent, glm::ivec2{ 0,-1 }, 25);
+	map->AddKeyboardAction<MoveCommand>(1, KeyState::keyPressed, SDL_SCANCODE_A, &parent, glm::ivec2{ -1,0 }, 25);
+	map->AddKeyboardAction<MoveCommand>(2, KeyState::keyPressed, SDL_SCANCODE_S, &parent, glm::ivec2{ 0,1 },	25);
+	map->AddKeyboardAction<MoveCommand>(3, KeyState::keyPressed, SDL_SCANCODE_D, &parent, glm::ivec2{ 1,0 },	25);
 
-	std::cout << "Info: \n";
-	std::cout << "Press 1 to detach child from parent\n";
-	std::cout << "Press 2 to reattach child to parent\n";
-	std::cout << "Press 3 to delete the child object\n";
-	std::cout << "Press 4 to delete the parent object\n";
+	map->AddKeyboardAction<TestCommand>(4, KeyState::keyUp, SDL_SCANCODE_1);
+	map->AddKeyboardAction<TestCommand>(5, KeyState::keyDown, SDL_SCANCODE_2);
+	map->AddKeyboardAction<TestCommand>(6, KeyState::keyPressed, SDL_SCANCODE_3);
+
+	map->AddGamePadAction<MoveCommand>(id, KeyState::keyPressed, GamePad::Button::dPadUp,		&parent1, glm::ivec2{ 0,-1 },	50);
+	map->AddGamePadAction<MoveCommand>(id, KeyState::keyPressed, GamePad::Button::dPadLeft,		&parent1, glm::ivec2{ -1,0 },	50);
+	map->AddGamePadAction<MoveCommand>(id, KeyState::keyPressed, GamePad::Button::dPadDown,		&parent1, glm::ivec2{ 0,1	},	50);
+	map->AddGamePadAction<MoveCommand>(id, KeyState::keyPressed, GamePad::Button::dPadRight,	&parent1, glm::ivec2{ 1,0 },	50);
 }
 
 int main(int, char* [])
@@ -84,7 +88,15 @@ int main(int, char* [])
 	settings.width = 640;
 	settings.height = 480;
 
-	dae::Minigin engine(settings);
-	engine.Run(load);
+	try
+	{
+		dae::Minigin engine(settings);
+		engine.Run(load);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what();
+	}
+
 	return 0;
 }
