f9a80560a82bf26d98170f514585a2e9baf5b961
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-19 10:44:08 +0100

    Added some helper functions for basic geometry

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -66,7 +66,7 @@ void dae::Renderer::RenderTexture(const Texture2D& texture, const float x, const
 
 void dae::Renderer::RenderTexture(const Texture2D& texture, const float x, const float y, const float width, const float height) const
 {
-	SDL_Rect dst{};
+	SDL_Rect dst;
 	dst.x = static_cast<int>(x);
 	dst.y = static_cast<int>(y);
 	dst.w = static_cast<int>(width);
@@ -80,3 +80,67 @@ void dae::Renderer::RenderTexture(const Texture2D& texture, SDL_Rect rect) const
 }
 
 SDL_Renderer* dae::Renderer::GetSDLRenderer() const { return m_pRenderer; }
+
+void dae::Renderer::RenderRectangle(const glm::ivec4& rect, bool isFilled, glm::u8vec4 color) const
+{
+	SDL_Color originalColor;
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+
+	const SDL_Rect sdlRect{ rect.x, rect.y, rect.z, rect.w };
+
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, color.a);
+	if (isFilled)
+		SDL_RenderFillRect(GetSDLRenderer(), &sdlRect);
+	else
+		SDL_RenderDrawRect(GetSDLRenderer(), &sdlRect);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+
+}
+
+void dae::Renderer::RenderPoint(int x, int y, glm::u8vec4 color) const
+{
+	SDL_Color originalColor;
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, color.a);
+	SDL_RenderDrawPoint(GetSDLRenderer(), x, y);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+}
+
+void dae::Renderer::RenderLine(int x1, int y1, int x2, int y2, glm::u8vec4 color) const
+{
+	SDL_Color originalColor;
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, 255);
+	SDL_RenderDrawLine(GetSDLRenderer(), x1, y1, x2, y2);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+}
+
+void dae::Renderer::RenderShape(const glm::ivec2* points, int count, glm::u8vec4 color) const
+{
+	SDL_Color originalColor;
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+
+	const auto sdlPoints = new SDL_Point[count];
+	for (int i = 0; i < count; ++i)
+		sdlPoints[i] = GlmToSDLPoint(points[i]);
+
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, 255);
+	SDL_RenderDrawLines(m_pRenderer, sdlPoints, count);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+
+}
+
+void dae::Renderer::RenderShape(const std::vector<glm::ivec2>& points, glm::u8vec4 color) const
+{
+	RenderShape(&points.front(), static_cast<int>(points.size()), color);
+}
+
+SDL_Point dae::Renderer::GlmToSDLPoint(const glm::ivec2& point)
+{
+	SDL_Point sdlPoint;
+	sdlPoint.x = point.x;
+	sdlPoint.y = point.y;
+	return sdlPoint;
+}
--- Minigin/Renderer.h
+++ Minigin/Renderer.h
@@ -25,10 +25,18 @@ namespace dae
 		const SDL_Color& GetBackgroundColor() const { return m_ClearColor; }
 		void SetBackgroundColor(const SDL_Color& color) { m_ClearColor = color; }
 
+		void RenderRectangle(const glm::ivec4& rect, bool isFilled, glm::u8vec4 color) const;
+		void RenderPoint(int x, int y, glm::u8vec4 color) const;
+		void RenderLine(int x1, int y1, int x2, int y2, glm::u8vec4 color) const;
+		void RenderShape(const glm::ivec2* points, int count, glm::u8vec4 color) const;
+		void RenderShape(const std::vector<glm::ivec2>& points, glm::u8vec4 color) const;
+
 	private:
 		SDL_Renderer* m_pRenderer{};
 		SDL_Window* m_pWindow{};
-		SDL_Color m_ClearColor{};	
+		SDL_Color m_ClearColor{};
+
+		static SDL_Point GlmToSDLPoint(const glm::ivec2& point);
 	};
 }
 
