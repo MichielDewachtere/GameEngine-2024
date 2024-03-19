02051eab0694f06298585a25343d1c09b9c511b7
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-19 10:40:57 +0100

    Quick test component for colliders

---
+++ Minigin/ColliderTest.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include "ColliderTest.h"
+
+#include "GameObject.h"
+
+ColliderTest::ColliderTest(dae::GameObject* pOwner)
+	: Component(pOwner)
+{
+}
+
+void ColliderTest::Start()
+{
+	m_Self = GetOwner()->GetComponent<dae::ColliderComponent>();
+}
+
+void ColliderTest::Update()
+{
+	if (m_Self->IsEntirelyOverlapping(*m_pOther, {2,2}))
+		dae::Logger::LogInfo({ "Game object {} is entirely overlapping with game object {}" }, GetOwner()->GetId(), m_pOther->GetOwner()->GetId());
+	else if (m_Self->IsOverlapping(*m_pOther))
+		dae::Logger::LogInfo({ "Game object {} is overlapping with game object {}" }, GetOwner()->GetId(), m_pOther->GetOwner()->GetId());
+}
+
+void ColliderTest::SetColliderToCheck(dae::ColliderComponent* pOther)
+{
+	m_pOther = pOther;
+}
