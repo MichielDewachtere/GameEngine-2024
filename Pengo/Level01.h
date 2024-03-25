9f4b497a818467c912d322a053fd236f54d61e74
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-25 09:18:53 +0100

    Update Level01.h

--- Pengo/Level01.h
+++ Pengo/Level01.h
@@ -2,11 +2,12 @@
 #define LEVEL01_H
 
 #include <Scene.h>
+#include <Minigin.h>
 
 class Level01 final : public dae::Scene
 {
 public:
-	explicit Level01();
+	explicit Level01(const dae::WindowSettings& settings);
 	~Level01() override = default;
 
 	Level01(const Level01& other) = delete;
@@ -15,6 +16,9 @@ public:
 	Level01& operator=(Level01&& rhs) = delete;
 
 	virtual void Load() override;
+
+private:
+	dae::WindowSettings m_Settings;
 };
 
 #endif // LEVEL01_H
