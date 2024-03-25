7977e2ec0a43714b2ff6ecff4be338a057d40d4b
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-25 09:13:45 +0100

    Added prefab Base Class

---
+++ Minigin/Prefab.cpp
@@ -0,0 +1,14 @@
+﻿#include "Prefab.h"
+
+#include "GameObject.h"
+#include "Scene.h"
+
+dae::Prefab::Prefab(GameObject* pOwner)
+{
+	m_pGameObject = &pOwner->CreateGameObject();
+}
+
+dae::Prefab::Prefab(Scene* pScene)
+{
+	m_pGameObject = &pScene->CreateGameObject();
+}
