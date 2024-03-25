4b9b9d8f9e826883322a6b3a1fd251bca8df8abb
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-25 09:18:50 +0100

    Update Level01.cpp

--- Pengo/Level01.cpp
+++ Pengo/Level01.cpp
@@ -1,10 +1,18 @@
 ﻿#include "Level01.h"
 
-Level01::Level01()
+#include "IcePrefab.h"
+#include "LevelGenerator.h"
+#include "LevelParser.h"
+#include "Macros.h"
+
+Level01::Level01(const dae::WindowSettings& settings)
 	: Scene("level01", "gameplay")
+	, m_Settings(settings)
 {
 }
 
 void Level01::Load()
 {
+	LevelGenerator::GetInstance().GenerateLevel(this, m_Settings, 1);
+	//LevelParser::ParseLevel(this, m_Settings, "../data/levels/level1-test.txt");
 }
