4e5e27620619317e69a42017af187f1db71aa5b0
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-25 09:16:54 +0100

    WIP, started working on maze component

---
+++ Pengo/Maze.cpp
@@ -0,0 +1,90 @@
+﻿#include "Maze.h"
+#include <memory>
+#include <iostream>
+
+#include "PlayerPrefab.h"
+#include "IcePrefab.h"
+#include "StarBlockPrefab.h"
+#include "HiddenEggPrefab.h"
+
+#include "Macros.h"
+#include "WallPrefab.h"
+
+Maze::Maze(dae::GameObject* pOwner, const glm::ivec2& size)
+	: Component(pOwner)
+{
+	m_Maze = std::vector(size.x, std::vector(size.y, BlockType::wall));
+}
+
+void Maze::Init() const
+{
+    const int mazeWidth = static_cast<int>(m_Maze.size());
+    const int mazeHeight = static_cast<int>(m_Maze.front().size());
+
+	// Add walls
+		// Horizontal
+	auto wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ WALL_WIDTH * PIXEL_SCALE, 0 }, true);
+    wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ WALL_WIDTH * PIXEL_SCALE, mazeHeight * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE }, true);
+		// Vertical
+    wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ 0, 0 }, false);
+    wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ mazeWidth * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE, 0 }, false);
+
+    // Add Blocks
+	for (size_t x = 0; x < m_Maze.size(); ++x)
+    {
+        for (size_t y = 0; y < m_Maze[x].size(); ++y)
+        {
+            //                                   Position                   Wall Offset
+            auto scaledPos = glm::vec2(x * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE, y * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE);
+
+
+            if (x == 6 && y == 6)
+            {
+                auto playerSpawn = std::make_unique<PlayerPrefab>(GetOwner(), scaledPos);
+                std::cout << 'u';
+            }
+
+            switch (m_Maze[x][y])
+            {
+            case BlockType::wall:
+            {
+                auto ice = std::make_unique<IcePrefab>(GetOwner(), scaledPos);
+                std::cout << "X";
+                break;
+            }
+            case BlockType::star:
+            {
+                auto starBlock = std::make_unique<StarBlockPrefab>(GetOwner(), scaledPos);
+                std::cout << "*";
+                break;
+            }
+            case BlockType::egg:
+            {
+                auto hiddenEgg = std::make_unique<HiddenEggPrefab>(GetOwner(), scaledPos);
+                std::cout << "!";
+                break;
+            }
+            case BlockType::air:
+            default:
+                std::cout << '_';
+            }
+        }
+
+        std::cout << '\n';
+    }
+}
+
+void Maze::SetBlock(const glm::ivec2& pos, BlockType type)
+{
+	m_Maze[pos.x][pos.y] = type;
+}
+
+Maze::BlockType Maze::GetBlock(const glm::ivec2& pos) const
+{
+	return m_Maze[pos.x][pos.y];
+}
+
+bool Maze::IsOccupied(const glm::ivec2& pos) const
+{
+	return m_Maze[pos.x][pos.y] != BlockType::air;
+}
---
+++ Pengo/Maze.h
@@ -0,0 +1,43 @@
+﻿#ifndef MAZE_H
+#define MAZE_H
+
+#include <glm/vec2.hpp>
+
+#include <Component.h>
+#include <vector>
+
+class Maze final : public dae::Component
+{
+public:
+	enum class BlockType : char
+	{
+		air = 0,
+		wall = 1,
+		star = 2,
+		egg = 3,
+	};
+
+	explicit Maze(dae::GameObject* pOwner, const glm::ivec2& size);
+	virtual ~Maze() override = default;
+
+	Maze(const Maze& other) = delete;
+	Maze& operator=(const Maze& rhs) = delete;
+	Maze(Maze&& other) = delete;
+	Maze& operator=(Maze&& rhs) = delete;
+
+	// This function should only be called to initialize blocks!
+	// Therefore it is best to only call it in the LevelGenerator class
+	void Init() const;
+
+	void SetBlock(const glm::ivec2& pos, BlockType type);
+	BlockType GetBlock(const glm::ivec2& pos) const;
+	bool IsOccupied(const glm::ivec2& pos) const;
+
+private:
+	// TODO: Stores entire maze, is owner of every object inside the maze
+	// Use this instead of collider components?
+	// TODO: store game objects too? Easy way of getting blocks?
+	std::vector<std::vector<BlockType>> m_Maze{};
+};
+
+#endif // MAZE_H
