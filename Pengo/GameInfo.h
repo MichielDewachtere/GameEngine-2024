6029bf4df6b47b9d90273bab299ef0b03a5ecab4
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 22:43:12 +0100

    Update GameInfo.h

--- Pengo/GameInfo.h
+++ Pengo/GameInfo.h
@@ -5,7 +5,9 @@
 
 struct Tags
 {
-	
+	inline static const std::string pengo = "Player";
+	inline static const std::string sno_bee = "Enemy";
+	inline static const std::string game = "Game";
 };
 
 struct Scenes
@@ -25,6 +27,8 @@ struct InputCommands
 	inline static constexpr uint8_t move_left = 2;
 	inline static constexpr uint8_t move_right = 3;
 	inline static constexpr uint8_t interact = 4;
+
+	inline static constexpr uint8_t print_maze = 100;
 };
 
 #endif // GAMEINFO_H
