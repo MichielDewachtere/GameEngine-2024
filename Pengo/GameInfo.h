c24a6facd9fb343f9a9fe02af0eefeb9b27c263c
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:09:33 +0100

    Update GameInfo.h

--- Pengo/GameInfo.h
+++ Pengo/GameInfo.h
@@ -1,6 +1,30 @@
 ﻿#ifndef GAMEINFO_H
 #define GAMEINFO_H
 
+#include <stdint.h>
 
+struct Tags
+{
+	
+};
+
+struct Scenes
+{
+	
+};
+
+struct InputMaps
+{
+	
+};
+
+struct InputCommands
+{
+	inline static constexpr uint8_t move_up = 0;
+	inline static constexpr uint8_t move_down = 1;
+	inline static constexpr uint8_t move_left = 2;
+	inline static constexpr uint8_t move_right = 3;
+	inline static constexpr uint8_t interact = 4;
+};
 
 #endif // GAMEINFO_H
