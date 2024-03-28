18b1d92008db38a6cd192cfdd8f5f6077d086183
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:10:38 +0100

    Added functionality for the hidden eggs (enemy spawns)

---
+++ Pengo/HiddenEgg.cpp
@@ -0,0 +1,40 @@
+﻿#include "HiddenEgg.h"
+
+#include <GameObject.h>
+#include <SpriteComponent.h>
+
+#include "Enemy.h"
+#include "IceBlock.h"
+
+HiddenEgg::HiddenEgg(dae::GameObject* pOwner)
+	: Component(pOwner)
+{
+}
+
+HiddenEgg::~HiddenEgg() = default;
+
+void HiddenEgg::Start()
+{
+	m_pSpriteComponent = GetOwner()->GetComponent<dae::SpriteComponent>();
+}
+
+void HiddenEgg::Update()
+{
+	if (m_EggPopped && m_pSpriteComponent->IsAnimationPlaying() == false)
+	{
+		GetOwner()->GetParent()->Destroy();
+	}
+}
+
+void HiddenEgg::PopEgg()
+{
+	m_EggPopped = true;
+
+	GetOwner()->GetParent()->GetComponent<IceBlock>()->Break();
+
+	if (m_pSpriteComponent == nullptr)
+		m_pSpriteComponent = GetOwner()->GetComponent<dae::SpriteComponent>();
+
+	m_pSpriteComponent->Enable();
+	m_pSpriteComponent->PlayAnimation(0, 2, 3);
+}
---
+++ Pengo/HiddenEgg.h
@@ -0,0 +1,32 @@
+﻿#ifndef HIDDENEGG_H
+#define HIDDENEGG_H
+
+#include <Component.h>
+
+namespace dae
+{
+	class SpriteComponent;
+}
+
+class HiddenEgg final : public dae::Component
+{
+public:
+	explicit HiddenEgg(dae::GameObject* pOwner);
+	virtual ~HiddenEgg() override;
+
+	HiddenEgg(const HiddenEgg& other) = delete;
+	HiddenEgg& operator=(const HiddenEgg& rhs) = delete;
+	HiddenEgg(HiddenEgg&& other) = delete;
+	HiddenEgg& operator=(HiddenEgg&& rhs) = delete;
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+	void PopEgg();
+
+private:
+	bool m_EggPopped{ false };
+	dae::SpriteComponent* m_pSpriteComponent{ nullptr };
+};
+
+#endif // HIDDENEGG_H
