4050ef28d516c585f385d7e36b7f93aba3479daa
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:09:02 +0100

    Started working on an enemy class
    Don't know yet how i will handle the enemy AI. That will probably be one of the last things I will do.

---
+++ Pengo/Enemy.cpp
@@ -0,0 +1,113 @@
+﻿#include "Enemy.h"
+
+#include <GameObject.h>
+#include <SpriteComponent.h>
+
+#include "HiddenEgg.h"
+#include "Macros.h"
+#include "Maze.h"
+#include "Move.h"
+#include "Pushable.h"
+
+std::map<Direction, std::pair<int, int>> Enemy::m_DirectionToSquashAnim
+{
+	{Direction::up, {32,33}},
+	{Direction::right, {34,35}},
+	{Direction::down, {36,37}},
+	{Direction::left, {38,39}}
+};
+
+
+Enemy::Enemy(dae::GameObject* pOwner)
+	: Component(pOwner)
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::Start()
+{
+	m_pMoveComponent = GetOwner()->GetComponent<Move>();
+	m_pMoveComponent->Animate(false);
+	//m_pMoveComponent->Disable();
+
+	m_pSpriteComponent = GetOwner()->GetComponent<dae::SpriteComponent>();
+	m_pSpriteComponent->PlayAnimation(0, 5, 0);
+
+	m_PushableComponent = GetOwner()->GetComponent<Pushable>();
+}
+
+void Enemy::Update()
+{
+	switch (m_CurrentState)
+	{
+	case EnemyState::spawned:
+	{
+		if (m_pSpriteComponent->IsAnimationPlaying() == false)
+		{
+			m_pSpriteComponent->PlayAnimation(8, 9);
+			m_CurrentState = EnemyState::moving;
+		}
+		break;
+	}
+	case EnemyState::moving:
+	{
+
+		break;
+	}
+	case EnemyState::pushed:
+	{
+		if (m_PushableComponent->IsBeingPushed())
+		{
+			m_pSpriteComponent->Pause(true);
+		}
+		else
+		{
+			if (m_pSpriteComponent->IsPaused())
+			{
+				m_pSpriteComponent->Pause(false);
+				m_pSpriteComponent->PlayAnimation(m_DirectionToSquashAnim[m_PushDirection].first,
+				                                  m_DirectionToSquashAnim[m_PushDirection].second, 
+				                                  0, 0.1f);
+			}
+			else if (m_pSpriteComponent->IsAnimationPlaying() == false)
+			{
+				m_CurrentState = EnemyState::died;
+				m_pSpriteComponent->Disable();
+			}
+		}
+		break;
+	}
+	case EnemyState::stunned:
+	{
+
+		break;
+	}
+	case EnemyState::died:
+	{
+		enemyDied.Notify();
+
+		//auto pos = m_pMoveComponent->GetMazePos();
+		//GetOwner()->GetParent()->GetComponent<Maze>()->SetBlock(pos, Maze::BlockType::air);
+
+		GetOwner()->Destroy();
+		break;
+	}
+	}
+}
+
+void Enemy::HandleEvent(dae::GameObjectEvent event)
+{
+	if (event == dae::GameObjectEvent::destroyed)
+		enemyDied.Destroy();
+}
+
+void Enemy::Push(Direction direction)
+{
+	m_CurrentState = EnemyState::pushed;
+	m_PushDirection = direction;
+	GetOwner()->GetComponent<Pushable>()->Push(direction);
+	m_pMoveComponent->SetMovementSpeed(PUSH_SPEED);
+}
---
+++ Pengo/Enemy.h
@@ -0,0 +1,60 @@
+﻿#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <Subject.h>
+#include <Component.h>
+
+namespace dae
+{
+	enum class GameObjectEvent : char;
+	class SpriteComponent;
+}
+
+class Pushable;
+enum class Direction : char;
+class Move;
+
+class Enemy final
+	: public dae::Component
+	, public dae::Observer<dae::GameObjectEvent>
+{
+public:
+	enum class EnemyState
+	{
+		spawned = 0,
+		moving = 1,
+		pushed = 2,
+		stunned = 3,
+		died = 4
+	};
+
+public:
+	explicit Enemy(dae::GameObject* pOwner);
+	virtual ~Enemy() override;
+
+	Enemy(const Enemy& other) = delete;
+	Enemy& operator=(const Enemy& rhs) = delete;
+	Enemy(Enemy&& other) = delete;
+	Enemy& operator=(Enemy&& rhs) = delete;
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+	virtual void HandleEvent(dae::GameObjectEvent) override;
+	virtual void OnSubjectDestroy() override {}
+
+	void Push(Direction direction);
+
+	dae::Subject<> enemyDied;
+
+private:
+	EnemyState m_CurrentState{ EnemyState::spawned };
+	dae::SpriteComponent* m_pSpriteComponent{ nullptr };
+	Move* m_pMoveComponent{ nullptr };
+	Pushable* m_PushableComponent{ nullptr };
+
+	Direction m_PushDirection{};
+	static std::map<Direction, std::pair<int, int>> m_DirectionToSquashAnim;
+};
+
+#endif // ENEMY_H
