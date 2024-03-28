5f1bf241d9a044af63261dec1084ec23aacdfb07
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:12:54 +0100

    Update Macros.h

--- Pengo/Macros.h
+++ Pengo/Macros.h
@@ -2,7 +2,14 @@
 #define MACROS_H
 
 #define PIXEL_SCALE 3
+
 #define BLOCK_SIZE 16
 #define WALL_WIDTH 8
+
+#define MAZE_WIDTH 13
+#define MAZE_HEIGHT 15
+
+#define PLAYER_SPEED 100.f
+#define PUSH_SPEED 300.f
 
 #endif // MACROS_H
