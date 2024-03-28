361730da3fcc3c357e66aba328bc6496ea4c264a
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:13:22 +0100

    Added Move Component for everything that moves

---
+++ Pengo/Move.cpp
@@ -0,0 +1,117 @@
+﻿#include "Move.h"
+
+#include <glm/glm.hpp>
+
+#include <GameObject.h>
+#include <GameTime.h>
+#include <SpriteComponent.h>
+
+#include "GameUtil.h"
+#include "Macros.h"
+#include "Maze.h"
+
+Move::Move(dae::GameObject* pOwner, const glm::ivec2& pos, Maze::BlockType type, float movementSpeed, bool animate)
+	: Component(pOwner)
+	, m_MazePosition(pos)
+	, m_Type(type)
+	, m_MovementSpeed(movementSpeed)
+	, m_Animate(animate)
+{
+	m_pMaze = GetOwner()->GetParent()->GetComponent<Maze>();
+	m_pMaze->SetBlock(m_MazePosition, m_Type, GetOwner());
+}
+
+void Move::Start()
+{
+	if (m_Animate)
+		m_pSpriteComponent = GetOwner()->GetComponent<dae::SpriteComponent>();
+}
+
+void Move::Update()
+{
+	if (m_Move == false)
+	{
+		if (m_pSpriteComponent && m_Animate)
+			m_pSpriteComponent->Pause(true);
+
+		return;
+	}
+
+	if (m_Animate && m_pSpriteComponent->IsAnimationPlaying() == false)
+	{
+		m_pSpriteComponent->PlayAnimation(m_DirectionToAnimation[m_Direction].first, m_DirectionToAnimation[m_Direction].second);
+	}
+
+	const auto dt = dae::GameTime::GetInstance().GetElapsed();
+	const glm::vec2 t{
+		static_cast<float>(m_DirectionVec.x) * m_MovementSpeed * dt,
+		static_cast<float>(m_DirectionVec.y) * m_MovementSpeed * dt
+	};
+
+	const auto transform = GetOwner()->GetTransform();
+	transform->Translate(t);
+
+	if (HasReachedPosition())
+	{
+		m_Move = false;
+
+		const auto pos = Maze::LocalToMaze(m_NewPosition);
+		m_pMaze->SetBlock(m_MazePosition, Maze::BlockType::air, nullptr);
+		m_MazePosition = pos;
+		m_pMaze->SetBlock(m_MazePosition, m_Type, GetOwner());
+
+		transform->SetLocalPosition(m_NewPosition);
+
+		if (m_MoveUntilStopped)
+		{
+			MoveInDirection(m_Direction, true);
+		}
+	}
+}
+
+bool Move::MoveInDirection(Direction dir, bool untilStopped)
+{
+	// TODO: maybe already record second move?
+	if (m_Move || IsActive() == false)
+		return false;
+
+	m_DirectionVec = DirectionToVector(dir);
+	const auto newPos = m_MazePosition + m_DirectionVec;
+
+	if (m_Direction != dir)
+	{
+		m_Direction = dir;
+	}
+
+	if (m_pSpriteComponent && m_Animate)
+	{
+		m_pSpriteComponent->PlayAnimation(m_DirectionToAnimation[m_Direction].first, m_DirectionToAnimation[m_Direction].second);
+
+		if (m_pSpriteComponent->IsPaused())
+			m_pSpriteComponent->Pause(false);
+	}
+
+	if (m_pMaze->IsOccupied(newPos))
+		return false;
+
+	const glm::vec2 scaledNewPos = Maze::MazeToLocal(newPos);
+
+	m_NewPosition = scaledNewPos;
+	m_Move = true;
+	m_MoveUntilStopped = untilStopped;
+
+	return true;
+}
+
+void Move::BindAnimationToDirection(Direction dir, const std::pair<int, int> indices)
+{
+	m_DirectionToAnimation[dir] = indices;
+}
+
+bool Move::HasReachedPosition() const
+{
+	const auto curPos = GetOwner()->GetTransform()->GetLocalPosition();
+
+	return std::abs(m_NewPosition.x - static_cast<int>(curPos.x)) < PIXEL_SCALE * 2
+		&& std::abs(static_cast<int>(curPos.y) - m_NewPosition.y) < PIXEL_SCALE * 2;
+}
---
+++ Pengo/Move.h
@@ -0,0 +1,64 @@
+﻿#ifndef MOVE_H
+#define MOVE_H
+
+#include <Component.h>
+#include <map>
+#include <vector>
+#include <glm/vec2.hpp>
+
+#include "Maze.h"
+
+namespace dae
+{
+	class SpriteComponent;
+}
+
+enum class Direction : char
+{
+	up = 0,
+	right = 1,
+	left = 2,
+	down = 3,
+	amountOfDirs = 4
+};
+
+class Move final : public dae::Component
+{
+public:
+	explicit Move(dae::GameObject* pOwner, const glm::ivec2& pos, Maze::BlockType type, float movementSpeed, bool animate);
+	virtual ~Move() override = default;
+
+	Move(const Move& other) = delete;
+	Move& operator=(const Move& rhs) = delete;
+	Move(Move&& other) = delete;
+	Move& operator=(Move&& rhs) = delete;
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+	bool MoveInDirection(Direction dir, bool untilStopped = false);
+	void BindAnimationToDirection(Direction dir, std::pair<int, int> indices);
+
+	void SetMovementSpeed(float speed) { m_MovementSpeed = speed; }
+
+	Direction GetDirection() const { return m_Direction; }
+	const glm::ivec2& GetMazePos() const { return m_MazePosition; }
+
+	bool IsMoving() const { return m_Move; }
+	void Animate(bool animate) { m_Animate = animate; }
+
+private:
+	glm::ivec2 m_MazePosition, m_NewPosition{}, m_DirectionVec{};
+	Direction m_Direction{ Direction::amountOfDirs };
+	Maze::BlockType m_Type;
+	float m_MovementSpeed;
+	bool m_Move{ false }, m_MoveUntilStopped{ false }, m_Animate;
+
+	Maze* m_pMaze{ nullptr };
+	dae::SpriteComponent* m_pSpriteComponent{ nullptr };
+	std::map<Direction, std::pair<int, int>> m_DirectionToAnimation;
+
+	bool HasReachedPosition() const;
+};
+
+#endif // MOVE_H
