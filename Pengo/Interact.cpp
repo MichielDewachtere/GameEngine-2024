908fe3b9b73f8898adce13d68713f9e73bcc1e14
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:11:23 +0100

    Added interact component for the player

---
+++ Pengo/Interact.cpp
@@ -0,0 +1,109 @@
+﻿// ReSharper disable CppDefaultCaseNotHandledInSwitchStatement
+// ReSharper disable CppIncompleteSwitchStatement
+// ReSharper disable CppClangTidyClangDiagnosticSwitch
+#include "Interact.h"
+
+#include <GameObject.h>
+#include <SpriteComponent.h>
+
+#include "GameUtil.h"
+#include "IceBlock.h"
+#include "Maze.h"
+#include "Move.h"
+#include "Pushable.h"
+#include "Wall.h"
+
+Interact::Interact(dae::GameObject* pOwner)
+	: Component(pOwner)
+{
+}
+
+void Interact::Start()
+{
+	m_pMaze = GetOwner()->GetParent()->GetComponent<Maze>();
+	m_pMove = GetOwner()->GetComponent<Move>();
+	m_pSpriteComponent = GetOwner()->GetComponent<dae::SpriteComponent>();
+}
+
+void Interact::Update()
+{
+	if (m_IsAnimating)
+	{
+		if (m_pSpriteComponent->IsAnimationPlaying() == false)
+		{
+			m_IsAnimating = false;
+
+			m_pMove->Enable();
+			m_pMove->Animate(true);
+		}
+	}
+}
+
+void Interact::AttemptInteraction()
+{
+	const auto direction = m_pMove->GetDirection();
+	const auto targetedPos = m_pMove->GetMazePos() + DirectionToVector(direction);
+	const auto targetedBlock = m_pMaze->GetBlockAndObject(targetedPos);
+
+	if (targetedBlock.first == Maze::BlockType::ice
+		|| targetedBlock.first == Maze::BlockType::star
+		|| targetedBlock.first == Maze::BlockType::egg)
+	{
+		const auto icePath = targetedPos + DirectionToVector(direction);
+
+		if (m_pMaze->IsOccupied(icePath) && targetedBlock.first != Maze::BlockType::star)
+		{
+			targetedBlock.second->GetComponent<IceBlock>()->Break();
+		}
+		else
+		{
+			targetedBlock.second->GetComponent<Pushable>()->Push(direction);
+		}
+
+		Animate(direction);
+	}
+	else if (targetedBlock.first == Maze::BlockType::wall)
+	{
+		switch (direction)
+		{
+		case Direction::up:
+			m_pMaze->GetWall(Maze::WallOrientation::north)->GetComponent<Wall>()->Shake();
+			break;
+		case Direction::right:
+			m_pMaze->GetWall(Maze::WallOrientation::east)->GetComponent<Wall>()->Shake();
+			break;
+		case Direction::left:
+			m_pMaze->GetWall(Maze::WallOrientation::west)->GetComponent<Wall>()->Shake();
+			break;
+		case Direction::down:
+			m_pMaze->GetWall(Maze::WallOrientation::south)->GetComponent<Wall>()->Shake();
+			break;
+		}
+
+		Animate(direction);
+	}
+}
+
+void Interact::Animate(Direction dir)
+{
+	m_pMove->Animate(false);
+	m_pMove->Disable();
+
+	m_IsAnimating = true;
+
+	switch (dir)
+	{
+	case Direction::up:
+		m_pSpriteComponent->PlayAnimation(12, 13, 1);
+		break;
+	case Direction::right:
+		m_pSpriteComponent->PlayAnimation(14, 15, 1);
+		break;
+	case Direction::left:
+		m_pSpriteComponent->PlayAnimation(10, 11, 1);
+		break;
+	case Direction::down:
+		m_pSpriteComponent->PlayAnimation(8, 9, 1);
+		break;
+	}
+}
---
+++ Pengo/Interact.h
@@ -0,0 +1,42 @@
+﻿#ifndef INTERACTCOMPONENT_H
+#define INTERACTCOMPONENT_H
+
+#include <Component.h>
+
+enum class Direction : char;
+
+namespace dae
+{
+	class SpriteComponent;
+}
+
+class Move;
+class Maze;
+
+class Interact final : public dae::Component
+{
+public:
+	explicit Interact(dae::GameObject* pOwner);
+	virtual ~Interact() override = default;
+
+	Interact(const Interact& other) = delete;
+	Interact& operator=(const Interact& rhs) = delete;
+	Interact(Interact&& other) = delete;
+	Interact& operator=(Interact&& rhs) = delete;
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+	void AttemptInteraction();
+
+private:
+	Maze* m_pMaze{ nullptr };
+	Move* m_pMove{ nullptr };
+	dae::SpriteComponent* m_pSpriteComponent{ nullptr };
+
+	bool m_IsAnimating{ false };
+
+	void Animate(Direction dir);
+};
+
+#endif // INTERACTCOMPONENT_H
