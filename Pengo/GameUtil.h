d852f18771afb85d21687ce55ea1c1aab3f355cc
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:10:01 +0100

    Create GameUtil.h
    Added converter function for the enum direction to a vector

---
+++ Pengo/GameUtil.h
@@ -0,0 +1,25 @@
+﻿#ifndef GAMEUTIL_H
+#define GAMEUTIL_H
+
+#include <glm/vec2.hpp>
+
+#include "Move.h"
+
+inline glm::ivec2 DirectionToVector(Direction dir)
+{
+	switch (dir)
+	{
+	case Direction::up:
+		return { 0,-1 };
+	case Direction::right:
+		return { 1,0 };
+	case Direction::left:
+		return { -1,0 };
+	case Direction::down:
+		return { 0,1 };
+	default:
+		return { 0,0 };
+	}
+}
+
+#endif // GAMEUTIL_H
