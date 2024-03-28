306d675b3137b24f77926c0704f5d4a538949912
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-28 18:13:58 +0100

    Added component for everything that can be pushed

---
+++ Pengo/Pushable.cpp
@@ -0,0 +1,52 @@
+﻿#include "Pushable.h"
+
+#include <GameObject.h>
+
+#include "Enemy.h"
+#include "GameUtil.h"
+#include "Move.h"
+#include "Maze.h"
+
+Pushable::Pushable(dae::GameObject* pOwner)
+	: Component(pOwner)
+{
+}
+
+void Pushable::Start()
+{
+	m_pMoveComponent = GetOwner()->GetComponent<Move>();
+	m_pMazeComponent = GetOwner()->GetParent()->GetComponent<Maze>();
+}
+
+void Pushable::Update()
+{
+	if (m_Pushed)
+	{
+		if (m_pMazeComponent->GetBlock(m_pMoveComponent->GetMazePos()) != Maze::BlockType::enemy)
+		{
+			const auto mazePos = m_pMoveComponent->GetMazePos();
+			const auto posToCheck = mazePos + DirectionToVector(m_Direction);
+			auto [type, go] = m_pMazeComponent->GetBlockAndObject(posToCheck);
+
+			if (type == Maze::BlockType::enemy)
+			{
+				go->GetComponent<Enemy>()->Push(m_Direction);
+			}
+		}
+
+		if (m_pMoveComponent->IsMoving() == false)
+			m_Pushed = false;
+	}
+}
+
+void Pushable::Push(Direction direction)
+{
+	m_pMoveComponent->MoveInDirection(direction, true);
+	m_Pushed = true;
+	m_Direction = direction;
+}
+
+bool Pushable::IsBeingPushed() const
+{
+	return m_Pushed;
+}
---
+++ Pengo/Pushable.h
@@ -0,0 +1,34 @@
+﻿#ifndef PUSHABLE_H
+#define PUSHABLE_H
+
+#include <Component.h>
+
+class Maze;
+class Move;
+enum class Direction : char;
+
+class Pushable final : public dae::Component
+{
+public:
+	explicit Pushable(dae::GameObject* pOwner);
+	virtual ~Pushable() override = default;
+
+	Pushable(const Pushable& other) = delete;
+	Pushable& operator=(const Pushable& rhs) = delete;
+	Pushable(Pushable&& other) = delete;
+	Pushable& operator=(Pushable&& rhs) = delete;
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+	void Push(Direction direction);
+	bool IsBeingPushed() const;
+
+private:
+	bool m_Pushed{ false };
+	Direction m_Direction{};
+	Move* m_pMoveComponent{ nullptr };
+	Maze* m_pMazeComponent{ nullptr };
+};
+
+#endif // PUSHABLE_H
