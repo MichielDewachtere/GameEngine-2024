0280250649acfa483c01d70f0933be7251d37bd8
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-29 20:55:04 +0100

    Update Maze.cpp

--- Pengo/Maze.cpp
+++ Pengo/Maze.cpp
@@ -1,7 +1,6 @@
 ﻿#include "Maze.h"
 
 #include <memory>
-#include <iostream>
 
 #include <GameObject.h>
 #include <InputManager.h>
@@ -30,23 +29,30 @@ void Maze::Init()
     const int mazeWidth = static_cast<int>(m_Maze.size());
     const int mazeHeight = static_cast<int>(m_Maze.front().size());
 
-	{
-		// Add walls
-    		// Horizontal
-    	auto wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ WALL_WIDTH * PIXEL_SCALE, 0 }, true);
+    {
+        // Add walls
+            // Horizontal
+        auto wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{WALL_WIDTH * PIXEL_SCALE, 0}, true,
+                                                 WallOrientation::north);
         m_pWalls[WallOrientation::north] = wall->GetGameObject();
-    	wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ WALL_WIDTH * PIXEL_SCALE, mazeHeight * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE }, true);
+        wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{
+	                                            WALL_WIDTH * PIXEL_SCALE,
+	                                            mazeHeight * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE
+                                            }, true, WallOrientation::south);
         m_pWalls[WallOrientation::south] = wall->GetGameObject();
-    		// Vertical
-    	wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ 0, 0 }, false);
+        // Vertical
+        wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{0, 0}, false,
+                                            WallOrientation::west);
         m_pWalls[WallOrientation::west] = wall->GetGameObject();
-    	wall = std::make_unique<WallPrefab>(GetOwner(), glm::vec2{ mazeWidth * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE, 0 }, false);
+        wall = std::make_unique<WallPrefab>(
+	        GetOwner(), glm::vec2{mazeWidth * BLOCK_SIZE * PIXEL_SCALE + WALL_WIDTH * PIXEL_SCALE, 0}, false,
+	        WallOrientation::east);
         m_pWalls[WallOrientation::east] = wall->GetGameObject();
-	}
+    }
 
 
     // Add Blocks
-	for (size_t x = 0; x < m_Maze.size(); ++x)
+    for (size_t x = 0; x < m_Maze.size(); ++x)
     {
         for (size_t y = 0; y < m_Maze[x].size(); ++y)
         {
@@ -55,12 +61,11 @@ void Maze::Init()
 
             // TODO: Second player spawn: [6][10]
             const bool needsPlayer = PlayerManager::GetInstance().RequestPlayer();
-            if (needsPlayer 
+            if (needsPlayer
                 && (x == 6 && y == 6 || x == 6 && y == 10))
             {
-	            const auto player = std::make_unique<PlayerPrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y });
-                PlayerManager::GetInstance().AddPlayer(player->GetGameObject());
-            	std::cout << 'u';
+                const auto player = std::make_unique<PlayerPrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y });
+                PlayerManager::GetInstance().AddPlayer(player->GetGameObject(), glm::ivec2{ x,y });
             }
 
             switch (m_Maze[x][y].first)
@@ -69,33 +74,25 @@ void Maze::Init()
             {
                 const auto ice = std::make_unique<IcePrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y });
                 m_Maze[x][y].second = ice->GetGameObject();
-                std::cout << "X";
                 break;
             }
             case BlockType::star:
             {
-	            const auto starBlock = std::make_unique<StarBlockPrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y });
+                const auto starBlock = std::make_unique<StarBlockPrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y });
                 m_Maze[x][y].second = starBlock->GetGameObject();
                 GetOwner()->GetComponent<StarBlockManager>()->AddStarBlock(starBlock->GetGameObject(), glm::ivec2{ x,y });
-                std::cout << "*";
                 break;
             }
             case BlockType::egg:
             {
-	            const auto iceBlock = std::make_unique<IcePrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y }, true);
-	            const auto hiddenEgg = std::make_unique<HiddenEggPrefab>(iceBlock->GetGameObject());
+                const auto iceBlock = std::make_unique<IcePrefab>(GetOwner(), scaledPos, glm::ivec2{ x,y }, true);
+                const auto hiddenEgg = std::make_unique<HiddenEggPrefab>(iceBlock->GetGameObject());
                 GetOwner()->GetComponent<EnemyHandler>()->AddEnemySpawn(hiddenEgg->GetGameObject());
                 m_Maze[x][y].second = iceBlock->GetGameObject();
-                std::cout << "!";
                 break;
             }
-            case BlockType::air:
-            default:
-                std::cout << '_';
             }
         }
-
-        std::cout << '\n';
     }
 }
 
@@ -147,6 +144,37 @@ std::pair<Maze::BlockType, dae::GameObject*> Maze::GetBlockAndObject(const glm::
     return m_Maze[pos.x][pos.y];
 }
 
+glm::ivec2 Maze::GetNearestFreePos(const glm::ivec2& pos) const
+{
+    if (m_Maze[pos.x][pos.y].first == BlockType::air)
+        return pos;
+
+    glm::ivec2 dir[]{
+        { 1, 0},{ 1, 1},{ 0, 1},{-1, 1},
+        {-1, 0},{-1,-1},{ 0,-1},{ 1,-1}
+    };
+
+    int counter = 1;
+    while (counter < 3)
+    {
+	    for (const auto & d : dir)
+	    {
+		    const auto posToCheck = pos + (d * counter);
+            if (IsInBounds(posToCheck) == false)
+                continue;
+
+            if (m_Maze[posToCheck.x][posToCheck.y].first == BlockType::air)
+            {
+	            return posToCheck;
+            }
+	    }
+
+        ++counter;
+    }
+
+    return {};
+}
+
 bool Maze::IsOccupied(const glm::ivec2& pos) const
 {
     if (const auto [isWall, orientation] = IsWall(pos); isWall)
@@ -157,6 +185,11 @@ bool Maze::IsOccupied(const glm::ivec2& pos) const
     const auto type = m_Maze[pos.x][pos.y].first;
     using enum BlockType;
     return !(type == air || type == player || type == enemy);
+}
+
+bool Maze::IsInBounds(const glm::ivec2& pos)
+{
+    return pos.x >= 0 && pos.x < MAZE_WIDTH && pos.y >= 0 && pos.y < MAZE_HEIGHT;
 }
 
 void Maze::PrintMaze() const
@@ -196,7 +229,6 @@ void Maze::PrintMaze() const
             case BlockType::none:
                 std::cout << '_';
                 break;
-            default:;
             }
         }
 
@@ -221,7 +253,7 @@ glm::ivec2 Maze::LocalToMaze(const glm::ivec2& localPos)
     };
 }
 
-std::pair<bool, Maze::WallOrientation> Maze::IsWall(const glm::ivec2& pos)
+std::pair<bool, WallOrientation> Maze::IsWall(const glm::ivec2& pos)
 {
     if (pos.x < 0)
         return { true, WallOrientation::west };
