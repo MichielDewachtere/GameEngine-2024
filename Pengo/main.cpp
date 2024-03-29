7eec6a9ac41d649336db6e541b96dc385e7c53c1
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-29 20:54:54 +0100

    Added all the levels + bonus time menu

--- Pengo/main.cpp
+++ Pengo/main.cpp
@@ -8,12 +8,18 @@
 #include <Minigin.h>
 #include <SceneManager.h>
 #include <ResourceManager.h>
+#include <InputManager.h>
 
+#include "BonusTimeMenu.h"
+#include "GameInfo.h"
 #include "LabScene.h"
-#include "Level01.h"
+#include "Level.h"
 #include "LevelGenerator.h"
 #include "Macros.h"
+#include "PlayerManager.h"
 #include "TestSecondScene.h"
+
+dae::WindowSettings g_windowSettings;
 
 void load()
 {
@@ -21,25 +27,61 @@ void load()
 
 	LevelGenerator::GetInstance().GenerateLevel();
 
-	SceneManager::GetInstance().CreateScene(new Level01());
+	auto& input = dae::InputManager::GetInstance();
+	input.AddInputMap(InputMaps::gameplay);
+	input.EnableGamePadInput(true);
+	input.SetInputMapActive(InputMaps::gameplay);
+
+	const auto idcs = input.RegisterGamePads();
+	if (idcs.empty())
+	{
+		PlayerManager::GetInstance().RegisterPlayer({ nullptr, {}, true, 255 });
+	}
+	else
+	{
+		for (const auto idx : idcs)
+		{
+			PlayerManager::GetInstance().RegisterPlayer({ nullptr, {}, false, idx });
+		}
+	}
+
+	// TODO: Add Menu's
+	SceneManager::GetInstance().CreateScene(new BonusTimeMenu(Scenes::bonus_menu, "none", g_windowSettings));
+
+	SceneManager::GetInstance().CreateScene(new Level("level1", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level2", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level3", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level4", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level5", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level6", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level7", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level8", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level9", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level10", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level11", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level12", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level13", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level14", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level15", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level16", InputMaps::gameplay));
+
 	SceneManager::GetInstance().CreateScene(new LabScene("labScene", "test"));
 	SceneManager::GetInstance().CreateScene(new TestSecondScene("secondScene", "secondScene"));
 
-	SceneManager::GetInstance().SetSceneActive("level01");
+	SceneManager::GetInstance().SetSceneActive("level1");
 }
 
 int main(int, char* [])
 {
-	dae::WindowSettings windowSettings;
-	windowSettings.windowTitle = "Prog 4 Engine";
-	windowSettings.dataPath = "../Data/";
-	windowSettings.fps = 60;
-	windowSettings.width = 224 * PIXEL_SCALE;
-	windowSettings.height = 272 * PIXEL_SCALE;
+	g_windowSettings.windowTitle = "Prog 4 Engine";
+	g_windowSettings.dataPath = "../Data/";
+	g_windowSettings.fps = 60;
+	g_windowSettings.width = 224 * PIXEL_SCALE;
+	g_windowSettings.height = 280 * PIXEL_SCALE;
 
 	try
 	{
-		dae::Minigin engine(windowSettings);
+		dae::Minigin engine(g_windowSettings);
 		engine.Run(load);
 	}
 	catch (std::exception& e)
