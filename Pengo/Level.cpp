1b48e3b003e7889ee6c5de0d8aac0ae55b14d7e9
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-03-29 20:54:35 +0100

    Added Level class instead of level01
    No need for seperate level class for each level. This class loads the level based on the scene name

---
+++ Pengo/Level.cpp
@@ -0,0 +1,28 @@
+﻿#include "Level.h"
+
+#include "InputManager.h"
+#include "LevelParser.h"
+
+Level::Level(std::string level, std::string inputMap, bool debug)
+	: Scene(std::move(level), std::move(inputMap))
+	, m_Debug(debug)
+{
+}
+
+void Level::Load()
+{
+	if (m_Debug)
+	{
+		LevelParser::ParseLevel(this, "../data/levels/debug.lvl", 5);
+	}
+	else
+	{
+		std::string levelPath = "../data/levels/";
+		levelPath += GetName();
+		levelPath += ".lvl";
+
+		const int difficulty = stoi(GetName().substr(5));
+
+		LevelParser::ParseLevel(this, levelPath, difficulty);
+	}
+}
---
+++ Pengo/Level.h
@@ -0,0 +1,23 @@
+﻿#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <Scene.h>
+
+class Level final : public dae::Scene
+{
+public:
+	explicit Level(std::string level, std::string inputMap, bool debug = false);
+	~Level() override = default;
+
+	Level(const Level& other) = delete;
+	Level& operator=(const Level& rhs) = delete;
+	Level(Level&& other) = delete;
+	Level& operator=(Level&& rhs) = delete;
+
+	virtual void Load() override;
+
+private:
+	bool m_Debug;
+};
+
+#endif // LEVEL_H
