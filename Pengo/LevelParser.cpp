7a115a1dcc413a42d19a2439a74c09de01c74e29
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-03 10:46:25 +0200

    Update LevelParser.cpp

--- Pengo/LevelParser.cpp
+++ Pengo/LevelParser.cpp
@@ -21,7 +21,7 @@ void LevelParser::ParseLevel(dae::Scene* pScene, const std::string& levelPath, i
 
 	// Create Level Object
 	auto& level = pScene->CreateGameObject(Tags::game);
-	level.GetTransform()->SetLocalPosition({ 0, BLOCK_SIZE * PIXEL_SCALE });
+	level.GetTransform()->SetLocalPosition({ 0, HUD_SIZE_TOP });
 	level.AddComponent<EnemyHandler>(difficulty);
 	const auto mazeComponent = level.AddComponent<Maze>(glm::ivec2{ MAZE_WIDTH, MAZE_HEIGHT});
 	level.AddComponent<StarBlockManager>();
