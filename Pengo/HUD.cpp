55258c2084689a51539d982ae845fd7a92ea3c8b
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-03 10:45:39 +0200

    Started working on the HUD

---
+++ Pengo/HUD.cpp
@@ -0,0 +1,86 @@
+﻿#include "HUD.h"
+
+#include <TextComponent.h>
+#include <Colors.h>
+#include <ResourceManager.h>
+#include <SceneManager.h>
+#include <TextureComponent.h>
+
+#include "ScoreDisplay.h"
+
+HUD::~HUD()
+{
+	dae::SceneManager::GetInstance().loadScene.RemoveObserver(this);
+	dae::SceneManager::GetInstance().exitScene.RemoveObserver(this);
+}
+
+void HUD::Init()
+{
+	// TODO:
+	InitHud();
+	// TOP:
+	//		Add Score player 1 AND 2
+	InitScoreDisplay();
+	////		Add High score
+	//InitHighScoreDisplay();
+	////		Add Lives
+	//InitLivesDisplay();
+	////		Amount of sno-bee eggs
+	//InitEnemyDisplay();
+	////		Add level (act) / 5
+	//InitLevelDisplayTop();
+	//// Bottom
+	////		Add level (act) % 5
+	//InitLevelDisplayBottom();
+
+	dae::SceneManager::GetInstance().loadScene.AddObserver(this);
+	dae::SceneManager::GetInstance().exitScene.AddObserver(this);
+}
+
+void HUD::HandleEvent(dae::SceneEvents event, dae::Scene* pScene)
+{
+	if (pScene->GetName().find("level") == std::string::npos)
+		return;
+
+	switch (event)
+	{
+		case dae::SceneEvents::exit:
+		{
+			m_pUniqueHud = pScene->GetUniqueGameObject(m_HudId);
+			break;
+		}
+		case dae::SceneEvents::load:
+		{
+			pScene->AddGameObject(std::move(m_pUniqueHud));
+			break;
+		}
+	}
+}
+
+void HUD::AddScore(ScoreEvents event) const
+{
+	m_pScoreDisplayPlayerOne->AddScore(event);
+}
+
+void HUD::InitHud()
+{
+	m_pUniqueHud = std::make_unique<dae::GameObject>(nullptr);
+	m_HudId = m_pUniqueHud->GetId();
+}
+
+void HUD::InitScoreDisplay()
+{
+	auto font = dae::ResourceManager::GetInstance().LoadFont("joystix-monospace.otf", 24);
+
+	auto& playerOne = m_pUniqueHud->CreateGameObject();
+	playerOne.AddComponent<dae::TextureComponent>();
+	playerOne.AddComponent<dae::TextComponent>("P1", std::move(font), dae::Colors::cyan);
+
+	font = dae::ResourceManager::GetInstance().LoadFont("joystix-monospace.otf", 24);
+
+	auto& scoreDisplay = playerOne.CreateGameObject();
+	scoreDisplay.GetTransform()->SetLocalPosition(50, 0);
+	scoreDisplay.AddComponent<dae::TextureComponent>();
+	scoreDisplay.AddComponent<dae::TextComponent>("0", std::move(font));
+	m_pScoreDisplayPlayerOne = scoreDisplay.AddComponent<ScoreDisplay>();
+}
---
+++ Pengo/HUD.h
@@ -0,0 +1,56 @@
+﻿#ifndef HUD_H
+#define HUD_H
+
+#include <memory>
+#include <Singleton.h>
+
+#include "Observer.h"
+
+namespace dae
+{
+	class Scene;
+	class GameObject;
+	enum class SceneEvents : char;
+}
+
+enum class ScoreEvents : char;
+class ScoreDisplay;
+
+class HUD final
+	: public dae::Singleton<HUD>
+	, public dae::Observer<dae::SceneEvents, dae::Scene*>
+{
+public:
+	virtual ~HUD() override;
+
+	HUD(const HUD& other) = delete;
+	HUD& operator=(const HUD& rhs) = delete;
+	HUD(HUD&& other) = delete;
+	HUD& operator=(HUD&& rhs) = delete;
+
+	virtual void Init() override;
+
+	virtual void HandleEvent(dae::SceneEvents, dae::Scene* pScene) override;
+	virtual void OnSubjectDestroy() override {}
+
+	void AddScore(ScoreEvents event) const;
+
+private:
+	friend class Singleton<HUD>;
+	explicit HUD() = default;
+
+	uint32_t m_HudId{};
+	std::unique_ptr<dae::GameObject> m_pUniqueHud{};
+	//dae::GameObject* m_pRawHud{};
+	ScoreDisplay* m_pScoreDisplayPlayerOne{ nullptr };
+
+	void InitHud();
+	void InitScoreDisplay();
+	//void InitHighScoreDisplay();
+	//void InitLivesDisplay();
+	//void InitEnemyDisplay();
+	//void InitLevelDisplayTop();
+	//void InitLevelDisplayBottom();
+};
+
+#endif // HUD_H
