89791c74d8039bcf372fb930f842363c7779afd7
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-23 11:55:16 +0200

    Transform component now supports 3d engine

--- RealCore/Transform.cpp
+++ RealCore/Transform.cpp
@@ -2,6 +2,12 @@
 
 #include <algorithm>
 
+#pragma warning(disable : 4201) // Disable warning 4201
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#pragma warning(default : 4201) // Restore the warning state to default
+
 #include "GameObject.h"
 
 real::Transform::Transform(GameObject* pOwner, glm::vec3 localPos)
@@ -23,7 +29,7 @@ void real::Transform::Update()
 		UpdateLocalPosition();
 }
 
-const glm::vec2& real::Transform::GetLocalPosition()
+const glm::vec3& real::Transform::GetLocalPosition()
 {
 	if (m_LocalNeedsUpdate)
 		UpdateLocalPosition();
@@ -33,11 +39,16 @@ const glm::vec2& real::Transform::GetLocalPosition()
 
 void real::Transform::SetLocalPosition(const glm::vec2& localPos)
 {
+	SetLocalPosition(glm::vec3{ localPos.x, localPos.y, m_LocalPosition.z });
+}
+
+void real::Transform::SetLocalPosition(const glm::vec3& localPos)
+{
 	m_LocalPosition = localPos;
 	SetWorldPositionDirty();
 
 	localPosChanged.Notify(TransformEvent::localPosChanged, m_WorldPosition);
-
+	m_WorldMatNeedsUpdate = true;
 	std::ranges::for_each(GetOwner()->GetChildren(), [](GameObject* go)
 		{
 			go->GetTransform()->SetLocalPositionDirty();
@@ -46,10 +57,15 @@ void real::Transform::SetLocalPosition(const glm::vec2& localPos)
 
 void real::Transform::SetLocalPosition(const float x, const float y)
 {
-	SetLocalPosition({ x,y });
+	SetLocalPosition(glm::vec3{ x,y, m_LocalPosition.z });
 }
 
-const glm::vec2& real::Transform::GetWorldPosition()
+void real::Transform::SetLocalPosition(float x, float y, float z)
+{
+	SetLocalPosition(glm::vec3{ x,y,z });
+}
+
+const glm::vec3& real::Transform::GetWorldPosition()
 {
 	if (m_WorldNeedsUpdate)
 		UpdateWorldPosition();
@@ -59,11 +75,16 @@ const glm::vec2& real::Transform::GetWorldPosition()
 
 void real::Transform::SetWorldPosition(const glm::vec2& worldPos)
 {
+	SetWorldPosition(glm::vec3{ worldPos.x, worldPos.y, m_WorldPosition.z });
+}
+
+void real::Transform::SetWorldPosition(const glm::vec3& worldPos)
+{
 	m_WorldPosition = worldPos;
 	SetLocalPositionDirty();
 
 	worldPosChanged.Notify(TransformEvent::worldPosChanged, m_WorldPosition);
-
+	m_WorldMatNeedsUpdate = true;
 	std::ranges::for_each(GetOwner()->GetChildren(), [](GameObject* go)
 		{
 			go->GetTransform()->SetWorldPositionDirty();
@@ -72,15 +93,26 @@ void real::Transform::SetWorldPosition(const glm::vec2& worldPos)
 
 void real::Transform::SetWorldPosition(const float x, const float y)
 {
-	SetWorldPosition({ x,y });
+	SetWorldPosition(glm::vec3{ x,y, m_WorldPosition.z });
+}
+
+void real::Transform::SetWorldPosition(float x, float y, float z)
+{
+	SetWorldPosition(glm::vec3{ x,y,z });
 }
 
 void real::Transform::Translate(const glm::vec2& translation)
+{
+	Translate(glm::vec3{ translation.x,translation.y,0 });
+}
+
+void real::Transform::Translate(const glm::vec3& translation)
 {
 	m_LocalPosition += translation;
 	localPosChanged.Notify(TransformEvent::localPosChanged, m_LocalPosition);
 
 	m_WorldNeedsUpdate = true;
+	m_WorldMatNeedsUpdate = true;
 	if (worldPosChanged.GetObservers().empty() == false)
 	{
 		GetWorldPosition();
@@ -90,23 +122,93 @@ void real::Transform::Translate(const glm::vec2& translation)
 
 void real::Transform::Translate(float translationX, float translationY)
 {
-	Translate({ translationX, translationY });
+	Translate({ translationX, translationY, 0 });
+}
+
+void real::Transform::Translate(float x, float y, float z)
+{
+	Translate({ x, y, z});
 }
 
 void real::Transform::SetUniformScale(float s)
 {
-	SetScale({ s,s });
+	SetScale({ s,s,s });
 }
 
 void real::Transform::SetScale(float x, float y)
 {
-	SetScale({ x,y });
+	SetScale(glm::vec3{ x,y,m_Scale.z });
+}
+
+void real::Transform::SetScale(float x, float y, float z)
+{
+	SetScale(glm::vec3{ x,y,z });
 }
 
 void real::Transform::SetScale(const glm::vec2& scale)
 {
+	SetScale({ scale.x, scale.y, m_Scale.z });
+}
+
+void real::Transform::SetScale(const glm::vec3& scale)
+{
 	m_Scale = scale;
+	m_WorldMatNeedsUpdate = true;
 	scaleChanged.Notify(TransformEvent::scaleChanged, scale);
+}
+
+void real::Transform::SetRotation(const glm::vec3& rotation, bool degrees)
+{
+	if (degrees)
+		m_Rotation = glm::radians(rotation);
+	else
+		m_Rotation = rotation;
+
+	m_WorldMatNeedsUpdate = true;
+}
+
+void real::Transform::SetPitch(const float pitch, bool degrees)
+{
+	if (degrees)
+		m_Rotation.x = glm::radians(pitch);
+	else
+		m_Rotation.x = pitch;
+
+	m_WorldMatNeedsUpdate = true;
+}
+
+void real::Transform::SetYaw(const float yaw, bool degrees)
+{
+	if (degrees)
+		m_Rotation.y = glm::radians(yaw);
+	else
+		m_Rotation.y = yaw;
+
+	m_WorldMatNeedsUpdate = true;
+}
+
+void real::Transform::SetRoll(const float roll, bool degrees)
+{
+	if (degrees)
+		m_Rotation.z = glm::radians(roll);
+	else
+		m_Rotation.z = roll;
+
+	m_WorldMatNeedsUpdate = true;
+}
+
+const glm::mat4& real::Transform::GetWorldMatrix()
+{
+	if (m_WorldMatNeedsUpdate)
+	{
+		const glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), GetWorldPosition());
+		const glm::mat4 rotationMatrix = glm::eulerAngleXYZ(m_Rotation.x, m_Rotation.y, m_Rotation.z);
+		const glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), m_Scale);
+
+		m_WorldMatrix = translationMatrix * rotationMatrix * scaleMatrix;
+	}
+
+	return m_WorldMatrix;
 }
 
 void real::Transform::UpdateLocalPosition()
@@ -152,18 +254,3 @@ void real::Transform::UpdateWorldPosition()
 
 	worldPosChanged.Notify(TransformEvent::worldPosChanged, m_WorldPosition);
 }
-
-//void real::Transform::SetRotation(float rotation)
-//{
-//	m_Rotation = rotation;
-//}
-//
-//void real::Transform::IncrementRotation(const float amount)
-//{
-//	m_Rotation += amount;
-//}
-//
-//void real::Transform::DecrementRotation(const float amount)
-//{
-//	m_Rotation -= amount;
-//}
