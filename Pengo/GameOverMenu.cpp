bb97cdc089b4bdb63ebec574dec34a7de65704c9
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-23 12:03:14 +0200

    Added GameOver Menu

---
+++ Pengo/GameOverMenu.cpp
@@ -0,0 +1,19 @@
+﻿#include "GameOverMenu.h"
+
+#include <TextureComponent.h>
+#include <ResourceManager.h>
+#include <TextComponent.h>
+
+GameOverMenu::GameOverMenu(std::string name, std::string inputMap)
+	: Scene(std::move(name), std::move(inputMap))
+{
+}
+
+void GameOverMenu::Load()
+{
+	auto pFont = real::ResourceManager::GetInstance().LoadFont("joystix-monospace.otf", 24);
+
+	auto& test = CreateGameObject();
+	test.AddComponent<real::TextureComponent>();
+	test.AddComponent<real::TextComponent>("End Screen", std::move(pFont));
+}
---
+++ Pengo/GameOverMenu.h
@@ -0,0 +1,23 @@
+﻿#ifndef GAMEOVERMENU_H
+#define GAMEOVERMENU_H
+
+#include <Scene.h>
+
+class GameOverMenu final : public real::Scene
+{
+public:
+	explicit GameOverMenu(std::string name, std::string inputMap);
+	virtual ~GameOverMenu() override = default;
+
+	GameOverMenu(const GameOverMenu& other) = delete;
+	GameOverMenu& operator=(const GameOverMenu& rhs) = delete;
+	GameOverMenu(GameOverMenu&& other) = delete;
+	GameOverMenu& operator=(GameOverMenu&& rhs) = delete;
+
+	virtual void Load() override;
+
+private:
+	
+};
+
+#endif // GAMEOVERMENU_H
