baf1e5106711cd173c776b121a01d884b7d7f73f
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-23 12:06:45 +0200

    Added command to start the game

---
+++ Pengo/StartGameCommand.cpp
@@ -0,0 +1,19 @@
+﻿#include "StartGameCommand.h"
+
+#include <SceneManager.h>
+
+StartGameCommand::StartGameCommand(int id, int controllerId)
+	: Command(id, controllerId)
+{
+}
+
+void StartGameCommand::Execute()
+{
+	if (m_IsFirstFrame)
+	{
+		m_IsFirstFrame = false;
+		return;
+	}
+
+	real::SceneManager::GetInstance().SetSceneActive("level1");
+}
---
+++ Pengo/StartGameCommand.h
@@ -0,0 +1,22 @@
+﻿#ifndef STARTGAMECOMMAND_H
+#define STARTGAMECOMMAND_H
+
+#include <Command.h>
+
+class StartGameCommand final : public real::Command
+{
+public:
+	explicit StartGameCommand(int id, int controllerId);
+	virtual ~StartGameCommand() override = default;
+
+	StartGameCommand(const StartGameCommand& other) = delete;
+	StartGameCommand& operator=(const StartGameCommand& rhs) = delete;
+	StartGameCommand(StartGameCommand&& other) = delete;
+	StartGameCommand& operator=(StartGameCommand&& rhs) = delete;
+
+	virtual void Execute() override;
+private:
+	static inline bool m_IsFirstFrame{ true };
+};
+
+#endif // STARTGAMECOMMAND_H
