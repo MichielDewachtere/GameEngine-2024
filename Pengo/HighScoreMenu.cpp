73bfe8b7653e1061c9ed3e549819372445d4f98e
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-24 15:34:05 +0200

    Created HighScoreMenu

---
+++ Pengo/HighScoreMenu.cpp
@@ -0,0 +1,1 @@
+﻿#include "HighScoreMenu.h"
---
+++ Pengo/HighScoreMenu.h
@@ -0,0 +1,19 @@
+﻿#ifndef HIGHSCOREMENU_H
+#define HIGHSCOREMENU_H
+
+class HighScoreMenu final
+{
+public:
+	explicit HighScoreMenu() = default;
+	~HighScoreMenu() = default;
+
+	HighScoreMenu(const HighScoreMenu& other) = delete;
+	HighScoreMenu& operator=(const HighScoreMenu& rhs) = delete;
+	HighScoreMenu(HighScoreMenu&& other) = delete;
+	HighScoreMenu& operator=(HighScoreMenu&& rhs) = delete;
+
+private:
+	
+};
+
+#endif // HIGHSCOREMENU_H
