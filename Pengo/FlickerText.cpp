f528d92a9c436cccea11d2c29c20bc56445fd98d
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-25 22:25:53 +0200

    Added Component for flashing text

---
+++ Pengo/FlickerText.cpp
@@ -0,0 +1,32 @@
+﻿#include "FlickerText.h"
+
+#include <GameObject.h>
+#include <TextComponent.h>
+#include <GameTime.h>
+#include <InputManager.h>
+
+FlickerText::FlickerText(real::GameObject* pOwner, glm::u8vec4 flickerColor, float flickerTime)
+	: Component(pOwner)
+	, m_FlickerTime(flickerTime)
+	, m_FlickerColor(std::move(flickerColor))
+{
+}
+
+void FlickerText::Start()
+{
+	m_pTextComponent = GetOwner()->GetComponent<real::TextComponent>();
+	m_OriginalColor = m_pTextComponent->GetColor();
+}
+
+void FlickerText::Update()
+{
+	m_AccuTime += real::GameTime::GetInstance().GetElapsed();
+
+	if (m_AccuTime >= m_FlickerTime)
+	{
+		m_pTextComponent->SetColor(m_IsOriginal ? m_OriginalColor : m_FlickerColor);
+
+		m_IsOriginal = !m_IsOriginal;
+		m_AccuTime = 0;
+	}
+}
---
+++ Pengo/FlickerText.h
@@ -0,0 +1,33 @@
+﻿#ifndef FLICKERTEXT_H
+#define FLICKERTEXT_H
+
+#include <Component.h>
+#include <glm/vec4.hpp>
+
+namespace real
+{
+	class TextComponent;
+}
+
+class FlickerText final : public real::Component
+{
+public:
+	explicit FlickerText(real::GameObject* pOwner, glm::u8vec4 flickerColor, float flickerTime);
+	virtual ~FlickerText() override = default;
+
+	FlickerText(const FlickerText& other) = delete;
+	FlickerText& operator=(const FlickerText& rhs) = delete;
+	FlickerText(FlickerText&& other) = delete;
+	FlickerText& operator=(FlickerText&& rhs) = delete;
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+private:
+	float m_FlickerTime, m_AccuTime{ 0 };
+	bool m_IsOriginal{ false };
+	glm::u8vec4 m_OriginalColor{}, m_FlickerColor;
+	real::TextComponent* m_pTextComponent{ nullptr };
+};
+
+#endif // FLICKERTEXT_H
