8412e3a2ce26d75883c8b4a4b34ab6629ea693f7
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-04-25 22:32:41 +0200

    Update Pengo

--- Pengo/main.cpp
+++ Pengo/main.cpp
@@ -17,8 +17,8 @@
 #include "Level.h"
 #include "LevelGenerator.h"
 #include "Macros.h"
-#include "PlayerManager.h"
 #include "StartMenu.h"
+#include "HighScoreMenu.h"
 
 real::WindowSettings g_windowSettings;
 
@@ -35,44 +35,26 @@ void load()
 	input.AddInputMap(InputMaps::bonus_time_menu);
 	input.AddInputMap(InputMaps::gameplay);
 	input.AddInputMap(InputMaps::game_over_menu);
+	input.AddInputMap(InputMaps::high_score_menu);
 	input.SetInputMapActive(InputMaps::start_menu);
-
-
-	//const auto idcs = input.RegisterGamePads();
-	//if (idcs.empty())
-	//{
-	//	PlayerManager::GetInstance().RegisterPlayer({ nullptr, {}, true, UCHAR_MAX });
-	//}
-	//else if (idcs.size() == 1)
-	//{
-	//	PlayerManager::GetInstance().RegisterPlayer({ nullptr, {}, true, UCHAR_MAX });
-	//	PlayerManager::GetInstance().RegisterPlayer({ nullptr, {}, false, idcs.front() });
-	//}
-	//else
-	//{
-	//	constexpr int maxAmountOfPlayers = 2;
-	//	for (int i = 0; i < maxAmountOfPlayers; ++i)
-	//	{
-	//		PlayerManager::GetInstance().RegisterPlayer({ nullptr, {}, false, idcs[i] });
-	//	}
-	//}
 
 	// Create the HUD
 	HUD::GetInstance().Init();
 
 	SceneManager::GetInstance().CreateScene(new StartMenu(Scenes::start_menu, "none", g_windowSettings));
 	SceneManager::GetInstance().CreateScene(new BonusTimeMenu(Scenes::bonus_menu, "none", g_windowSettings));
+	SceneManager::GetInstance().CreateScene(new HighScoreMenu(Scenes::high_score_menu, InputMaps::high_score_menu, g_windowSettings));
 	SceneManager::GetInstance().CreateScene(new GameOverMenu(Scenes::game_over_menu, "none", g_windowSettings));
 
 	SceneManager::GetInstance().CreateScene(new Level("level1", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level2", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level3", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level4", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level5", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level6", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level7", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level8", InputMaps::gameplay, true));
-	SceneManager::GetInstance().CreateScene(new Level("level9", InputMaps::gameplay, true));
+	SceneManager::GetInstance().CreateScene(new Level("level2", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level3", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level4", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level5", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level6", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level7", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level8", InputMaps::gameplay));
+	SceneManager::GetInstance().CreateScene(new Level("level9", InputMaps::gameplay));
 	SceneManager::GetInstance().CreateScene(new Level("level10", InputMaps::gameplay));
 	SceneManager::GetInstance().CreateScene(new Level("level11", InputMaps::gameplay));
 	SceneManager::GetInstance().CreateScene(new Level("level12", InputMaps::gameplay));
@@ -89,21 +71,21 @@ int main(int, char* [])
 	g_windowSettings.windowTitle = "Prog 4 Engine";
 	g_windowSettings.dataPath = "../Data/";
 	g_windowSettings.fps = 60;
-	g_windowSettings.width = BLOCK_SIZE * (MAZE_WIDTH + 1 /*2 walls*/) * PIXEL_SCALE;
-	g_windowSettings.height = BLOCK_SIZE * PIXEL_SCALE * (MAZE_HEIGHT + 1 /* 2 walls */) + HUD_SIZE_BOTTOM + HUD_SIZE_TOP;
+	g_windowSettings.width = WINDOW_WIDTH;
+	g_windowSettings.height = WINDOW_HEIGHT;
 
-	//real::RealEngine engine(g_windowSettings);
-	//engine.Run(load);
+	real::RealEngine engine(g_windowSettings);
+	engine.Run(load);
 
-	try
-	{
-		real::RealEngine engine(g_windowSettings);
-		engine.Run(load);
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what();
-	}
+	//try
+	//{
+	//	real::RealEngine engine(g_windowSettings);
+	//	engine.Run(load);
+	//}
+	//catch (std::exception& e)
+	//{
+	//	std::cout << e.what();
+	//}
 
 	return 0;
 }
