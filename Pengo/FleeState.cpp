0c44226794e9ee2270c465461239b40ff4aab8b0
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-03 11:57:59 +0200

    Added Flee State
    Once a single sno-bee is left, and the 60 seconds mark is hit. The last remaining sno bee flees to a corner and then the act is finished.

---
+++ Pengo/FleeState.cpp
@@ -0,0 +1,106 @@
+﻿#include "FleeState.h"
+
+#include <random>
+
+#include <GameObject.h>
+#include <Utils.h>
+
+#include "GameUtil.h"
+#include "IceBlock.h"
+#include "Move.h" 
+#include "SpawnState.h"
+
+FleeState::FleeState(real::GameObject* pOwner)
+	: IceBreakState(pOwner)
+{
+}
+
+void FleeState::Enter()
+{
+	IceBreakState::Enter();
+
+	GetMoveComponent()->BindAnimationToDirection(Direction::down, { 24,25 });
+	GetMoveComponent()->BindAnimationToDirection(Direction::left, { 26,27 });
+	GetMoveComponent()->BindAnimationToDirection(Direction::up, { 28,29 });
+	GetMoveComponent()->BindAnimationToDirection(Direction::right, { 30,31 });
+
+	const auto pos = GetMoveComponent()->GetMazePos();
+	if (pos.x > 6)
+		m_ClosestCorner.x = 12;
+
+	if (pos.y > 7)
+		m_ClosestCorner.y = 14;
+
+	int counter = 0;
+	while (IsValidCorner(m_ClosestCorner) == false)
+	{
+		m_ClosestCorner = possible_corners[counter];
+		++counter;
+	}
+}
+
+IEnemyState* FleeState::Update()
+{
+	if (GetMoveComponent()->IsMoving() == false)
+	{
+		const auto mazePos = GetMoveComponent()->GetMazePos();
+		if (m_ClosestCorner == mazePos)
+			return new SpawnState(GetOwner(), true);
+
+		MoveEnemy();
+	}
+
+	return nullptr;
+}
+
+void FleeState::Exit()
+{
+	IceBreakState::Exit();
+}
+
+void FleeState::MoveEnemy() const
+{
+	const auto mazePos = GetMoveComponent()->GetMazePos();
+
+	std::vector<Direction> directions;
+	std::map<Direction, bool> isIce;
+	if (m_ClosestCorner.x < mazePos.x)
+		isIce[Direction::left] = GetPoints(Direction::left, directions, false);
+	else
+		isIce[Direction::right] = GetPoints(Direction::right, directions, false);
+	if (m_ClosestCorner.y < mazePos.y)
+		isIce[Direction::up] = GetPoints(Direction::up, directions, false);
+	else
+		isIce[Direction::down] = GetPoints(Direction::down, directions, false);
+
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::ranges::shuffle(directions, g);
+
+	if (directions.empty())
+	{
+		GetMoveComponent()->MoveInDirection(/*InvertDirection(*/GetMoveComponent()->GetDirection()/*)*/);
+	}
+	else
+	{
+		const auto dirToGo = directions.front();
+
+		if (isIce[dirToGo])
+		{
+			const auto icePos = GetMoveComponent()->GetMazePos() + DirectionToVector(dirToGo);
+			const auto iceBlock = GetMazeComponent()->GetBlockAndObject(icePos).second.front();
+
+			iceBlock->GetComponent<IceBlock>()->Break();
+		}
+
+		GetMoveComponent()->MoveInDirection(dirToGo);
+	}
+}
+
+bool FleeState::IsValidCorner(const glm::ivec2& corner) const
+{
+	if (GetMazeComponent()->GetBlock(corner) == Maze::BlockType::star)
+		return false;
+
+	return true;
+}
---
+++ Pengo/FleeState.h
@@ -0,0 +1,39 @@
+﻿#ifndef FLEESTATE_H
+#define FLEESTATE_H
+
+#include <array>
+
+#include "IceBreakState.h"
+
+
+class FleeState final : public IceBreakState
+{
+public:
+	explicit FleeState(real::GameObject* pOwner);
+	virtual ~FleeState() override = default;
+
+	FleeState(const FleeState& other) = delete;
+	FleeState& operator=(const FleeState& rhs) = delete;
+	FleeState(FleeState&& other) = delete;
+	FleeState& operator=(FleeState&& rhs) = delete;
+
+	virtual void Enter() override;
+	virtual IEnemyState* Update() override;
+	virtual void Exit() override;
+
+private:
+
+	glm::ivec2 m_ClosestCorner{ 0,0 };
+
+	virtual void MoveEnemy() const override;
+	bool IsValidCorner(const glm::ivec2& corner) const;
+
+	static constexpr inline std::array<glm::ivec2, 4> possible_corners = {
+		glm::ivec2{0,0},
+		glm::ivec2{0,14},
+		glm::ivec2{12,0},
+		glm::ivec2{12,14},
+	};
+};
+
+#endif // FLEESTATE_H
