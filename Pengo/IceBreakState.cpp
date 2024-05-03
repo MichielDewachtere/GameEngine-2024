43662f8d921e8ef33c20d9d577e1c05f81db893d
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-05-03 12:00:29 +0200

    Added ice break state.
    When 30 seconds have passed, enemies enter an ice break state. It has all the abilities from move state, but enemies can now break ice blocks.

---
+++ Pengo/IceBreakState.cpp
@@ -0,0 +1,79 @@
+﻿#include "IceBreakState.h"
+
+#include <GameObject.h>
+#include <random>
+
+#include "EnemyHandler.h"
+#include "Move.h"
+#include "Game.h"
+#include "GameUtil.h"
+#include "IceBlock.h"
+#include "FleeState.h"
+
+IceBreakState::IceBreakState(real::GameObject* pOwner)
+	: MoveState(pOwner)
+{
+}
+
+void IceBreakState::Enter()
+{
+	MoveState::Enter();
+
+	GetMoveComponent()->BindAnimationToDirection(Direction::down, { 16,17 });
+	GetMoveComponent()->BindAnimationToDirection(Direction::left, { 18,19 });
+	GetMoveComponent()->BindAnimationToDirection(Direction::up, { 20,21 });
+	GetMoveComponent()->BindAnimationToDirection(Direction::right, { 22,23 });
+}
+
+IEnemyState* IceBreakState::Update()
+{
+	CheckForPlayer();
+
+	if (GetMoveComponent()->IsMoving() == false)
+	{
+		MoveEnemy();
+	}
+
+	if (Game::GetGameTime() > 60 && EnemyHandler::GetAmountOfEnemies() == 1)
+		return new FleeState(GetOwner());
+
+	return nullptr;
+}
+
+void IceBreakState::Exit()
+{
+	MoveState::Exit();
+}
+
+void IceBreakState::MoveEnemy() const
+{
+	std::vector<Direction> directions;
+	std::map<Direction, bool> isIce;
+	isIce[Direction::left] = GetPoints(Direction::left, directions, false);
+	isIce[Direction::right] = GetPoints(Direction::right, directions, false);
+	isIce[Direction::up] = GetPoints(Direction::up, directions, false);
+	isIce[Direction::down] = GetPoints(Direction::down, directions, false);
+
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::ranges::shuffle(directions, g);
+
+	if (directions.empty())
+	{
+		GetMoveComponent()->MoveInDirection(/*InvertDirection(*/GetMoveComponent()->GetDirection()/*)*/);
+	}
+	else
+	{
+		const auto dirToGo = directions.front();
+
+		if (isIce[dirToGo])
+		{
+			const auto icePos = GetMoveComponent()->GetMazePos() + DirectionToVector(dirToGo);
+			const auto iceBlock = GetMazeComponent()->GetBlockAndObject(icePos).second.front();
+
+			iceBlock->GetComponent<IceBlock>()->Break();
+		}
+
+		GetMoveComponent()->MoveInDirection(dirToGo);
+	}
+}
---
+++ Pengo/IceBreakState.h
@@ -0,0 +1,26 @@
+﻿#ifndef ICEBREAKSTATE_H
+#define ICEBREAKSTATE_H
+
+#include "MoveState.h"
+
+class IceBreakState : public MoveState
+{
+public:
+	explicit IceBreakState(real::GameObject* pOwner);
+	virtual ~IceBreakState() override = default;
+
+	IceBreakState(const IceBreakState& other) = delete;
+	IceBreakState& operator=(const IceBreakState& rhs) = delete;
+	IceBreakState(IceBreakState&& other) = delete;
+	IceBreakState& operator=(IceBreakState&& rhs) = delete;
+
+	virtual void Enter() override;
+	virtual IEnemyState* Update() override;
+	virtual void Exit() override;
+
+private:
+	virtual void MoveEnemy() const override;
+
+};
+
+#endif // ICEBREAKSTATE_H
