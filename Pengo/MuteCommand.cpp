90232eca52b81a9bb87d6aa45bccba7ff0622754
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-06-02 09:26:14 +0200

    Added a command to mute the game

---
+++ Pengo/MuteCommand.cpp
@@ -0,0 +1,14 @@
+﻿#include "MuteCommand.h"
+
+#include "Locator.h"
+
+MuteCommand::MuteCommand(int id, int controllerId)
+	: Command(id, controllerId)
+{
+}
+
+void MuteCommand::Execute()
+{
+	m_IsMuted = !m_IsMuted;
+	real::Locator::GetAudioSystem().Mute(m_IsMuted);
+}
---
+++ Pengo/MuteCommand.h
@@ -0,0 +1,23 @@
+﻿#ifndef MUTECOMMAND_H
+#define MUTECOMMAND_H
+
+#include <Command.h>
+
+class MuteCommand final : public real::Command
+{
+public:
+	explicit MuteCommand(int id, int controllerId);
+	virtual ~MuteCommand() override = default;
+
+	MuteCommand(const MuteCommand& other) = delete;
+	MuteCommand& operator=(const MuteCommand& rhs) = delete;
+	MuteCommand(MuteCommand&& other) = delete;
+	MuteCommand& operator=(MuteCommand&& rhs) = delete;
+
+	virtual void Execute() override;
+
+private:
+	bool m_IsMuted{ false };
+};
+
+#endif // MUTECOMMAND_H
