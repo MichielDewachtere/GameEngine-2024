d80c775ff8872656512aaa0db281c3223a82dd53
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-06-02 09:26:30 +0200

    Added a command to restart the game

---
+++ Pengo/RestartCommand.cpp
@@ -0,0 +1,25 @@
+﻿#include "RestartCommand.h"
+
+#include <SceneManager.h>
+
+#include "Game.h"
+#include "GameInfo.h"
+#include "HUD.h"
+#include "Locator.h"
+#include "PlayerManager.h"
+
+RestartCommand::RestartCommand(int id, int controllerId)
+	: Command(id, controllerId)
+{
+}
+
+void RestartCommand::Execute()
+{
+	Game::Reset();
+
+	HUD::GetInstance().Reset();
+	PlayerManager::GetInstance().Reset();
+
+	real::Locator::GetAudioSystem().StopAllSounds();
+	real::SceneManager::GetInstance().SetSceneActive(Scenes::start_menu);
+}
---
+++ Pengo/RestartCommand.h
@@ -0,0 +1,21 @@
+﻿#ifndef RESTARTCOMMAND_H
+#define RESTARTCOMMAND_H
+
+#include <Command.h>
+
+class RestartCommand final : public real::Command
+{
+public:
+	explicit RestartCommand(int id, int controllerId);
+	virtual ~RestartCommand() override = default;
+
+	RestartCommand(const RestartCommand& other) = delete;
+	RestartCommand& operator=(const RestartCommand& rhs) = delete;
+	RestartCommand(RestartCommand&& other) = delete;
+	RestartCommand& operator=(RestartCommand&& rhs) = delete;
+
+	virtual void Execute() override;
+
+};
+
+#endif // RESTARTCOMMAND_H
