da1fcd3d0346941855cdf0c6cc16f7c5a502d425
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2024-06-02 09:44:38 +0200

    Added skip level command

---
+++ Pengo/SkipLevelCommand.cpp
@@ -0,0 +1,25 @@
+﻿#include "SkipLevelCommand.h"
+
+#include "Game.h"
+#include "GameInfo.h"
+#include "SceneManager.h"
+
+SkipLevelCommand::SkipLevelCommand(int id, int controllerId)
+	: Command(id, controllerId)
+{
+}
+
+void SkipLevelCommand::Execute()
+{
+	auto level= Game::GetCurrentLevel();
+	m_LastLevel = level;
+	++level;
+
+	std::string levelName;
+	if (m_LastLevel == 16)
+		levelName = Scenes::high_score_menu;
+	else
+		levelName = "level" + std::to_string(level);
+
+	real::SceneManager::GetInstance().SetSceneActive(levelName);
+}
---
+++ Pengo/SkipLevelCommand.h
@@ -0,0 +1,23 @@
+﻿#ifndef SKIPLEVELCOMMAND_H
+#define SKIPLEVELCOMMAND_H
+
+#include <Command.h>
+
+class SkipLevelCommand final : public real::Command
+{
+public:
+	explicit SkipLevelCommand(int id, int controllerId);
+	virtual ~SkipLevelCommand() override = default;
+
+	SkipLevelCommand(const SkipLevelCommand& other) = delete;
+	SkipLevelCommand& operator=(const SkipLevelCommand& rhs) = delete;
+	SkipLevelCommand(SkipLevelCommand&& other) = delete;
+	SkipLevelCommand& operator=(SkipLevelCommand&& rhs) = delete;
+
+	void Execute() override;
+
+private:
+	int m_LastLevel{ -1 };
+};
+
+#endif // SKIPLEVELCOMMAND_H
